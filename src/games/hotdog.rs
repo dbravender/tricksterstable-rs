0d57f8e5b48016034ccdf592ecfa56f04ba4907f
Dan Bravender <dan@bravender.net>
2024-06-20 23:53:15 -0400

    Hotdog will be the next game!

---
+++ src/games/hotdog.rs
@@ -0,0 +1,5 @@
+/*
+Game: Hotdog
+Designer: Sean Ross
+BoardGameGeek: https://boardgamegeek.com/boardgame/365349/hotdog
+*/
--- src/games/mod.rs
+++ src/games/mod.rs
@@ -1,4 +1,5 @@
 pub mod dealers_dilemma;
+pub mod hotdog;
 pub mod kaibosh;
 pub mod szs;
 pub mod yokai2p;
