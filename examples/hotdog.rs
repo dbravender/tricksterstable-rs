7a752beefe23fa076549560209c1e209b1cfb75f
Dan Bravender <dan@bravender.net>
2024-08-29 17:54:49 -0400

    Hotdog #14 make experimental reward function the new default

--- examples/hotdog.rs
+++ examples/hotdog.rs
@@ -14,14 +14,15 @@ fn main() {
                 _ => false,
             };
 
-            let iterations = 1000;
-            let action = if game.current_player == 0 {
+            let mut iterations = 1000;
+            let action = if game.current_player != 0 {
                 let mut game = game.clone();
                 game.experiment = false;
                 get_mcts_move(&game, iterations, debug)
             } else {
                 let mut game = game.clone();
                 game.experiment = true;
+                iterations = 2000;
                 get_mcts_move(&game, iterations, debug)
             };
 
