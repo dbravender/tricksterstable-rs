28aa6e230cc87536ed536d8c0fbfa21934ae4d41
Dan Bravender <dan@bravender.net>
2024-09-28 13:55:29 -0400

    Kansas City: add MCTS comparison against random

--- examples/kansascity.rs
+++ examples/kansascity.rs
@@ -1,11 +1,11 @@
-use rand::{thread_rng, Rng};
+use rand::{seq::SliceRandom, thread_rng, Rng};
 use tricksterstable_rs::games::kansascity::{get_mcts_move, KansasCityGame};
 
 fn main() {
     let mut rnd = thread_rng();
     for _ in 0..1000 {
         let mut game = KansasCityGame::new();
-        game.dealer = if rnd.gen_range(0..100) > 50 { 0 } else { 1 };
+        game.dealer = rnd.gen_range(0..4);
         game.current_player = game.dealer;
         //println!("{:?}", &game);
         game.round = 5; // force single hand
@@ -14,18 +14,32 @@ fn main() {
             let action = if game.current_player == 0 || game.current_player == 2 {
                 let mut game = game.clone();
                 game.experiment = false;
-                iterations = 10;
-                get_mcts_move(&game, iterations, false)
+                let mut moves = game.get_moves();
+                moves.shuffle(&mut thread_rng());
+                moves[0]
+                //get_mcts_move(&game, iterations, false)
             } else {
                 let mut game = game.clone();
                 game.experiment = true;
-                iterations = 2000;
+                iterations = 500;
                 get_mcts_move(&game, iterations, false)
             };
 
             game.apply_move(action);
         }
-        println!("{:?}", &game);
-        println!("{:?} ", &game.scores)
+        let max_score = game.scores.iter().max().unwrap();
+        let winners: Vec<usize> = game
+            .scores
+            .iter()
+            .enumerate()
+            .filter(|(_player, score)| *score == max_score)
+            .map(|(player, _score)| player)
+            .collect();
+        for winner in winners {
+            println!("winner: {}", winner);
+        }
+        for (player, score) in game.scores.iter().enumerate() {
+            println!("score {}: {}", player, score);
+        }
     }
 }
