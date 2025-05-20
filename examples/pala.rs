4e155b0ec4629cfa0e7bcdf219c71cd87fd46e5d
Dan Bravender <dan@bravender.net>
2025-05-20 09:28:55 -0400

    #47 initial bot versus random players

--- examples/pala.rs
+++ examples/pala.rs
@@ -1,11 +1,18 @@
+use rand::{seq::SliceRandom, thread_rng};
 use tricksterstable_rs::games::pala::{get_mcts_move, PalaGame};
 
 fn main() {
+    let mut rng = thread_rng();
     for _ in 0..100 {
         let mut game = PalaGame::new();
         while game.winner.is_none() {
             let iterations = 1000;
-            let action = get_mcts_move(&game, iterations, false);
+            let action = if [1, 2, 3].contains(&game.current_player) {
+                let actions = game.get_moves();
+                *actions.choose(&mut rng).unwrap()
+            } else {
+                get_mcts_move(&game, iterations, false)
+            };
             game.apply_move(action);
         }
         println!("winner: {:?}", game.winner);
